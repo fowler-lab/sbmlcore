fd76caff84ee5deb3e44d1c1128075007315c262
Philip W Fowler <philipwfowler@users.noreply.github.com>
2022-02-02 11:08:50 +0000

    jupyter

---
+++ sbmlcore/CommandLineToolProperties.py
@@ -0,0 +1,11 @@
+import pandas
+import pathlib
+
+class CommandLineToolProperty(object):
+
+
+class StrideSecondaryStructure(CommandLineToolProperty):
+
+    def __init__(self, filename):
+
+        assert path.Path(filename).exists()
