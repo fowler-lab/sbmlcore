41a30675a1ee7dd7479aca7a2f010d10395e6252
Charlotte Lynch <charlotte.isabella.lynch@gmail.com>
2022-02-09 11:44:26 +0000

    Beginnings of structural features

---
+++ tests/test_MDAnalysis.py
@@ -0,0 +1,11 @@
+# Tests for MDAnalysis-calculated properties
+# Properties to test: COM, position of ions/DNA/mRNA - make this general
+import pandas
+import numpy
+import pytest
+import sbmlcore
+
+def test_MDAnalysis_import():
+
+def test_pncA_COM():
+    
