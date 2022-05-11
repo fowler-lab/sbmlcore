923e7e16307ad16a76d32a4829d5baab34e21726
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2022-05-11 17:13:27 +0100

    tidy

--- sbmlcore/TrajectoryDistances.py
+++ sbmlcore/TrajectoryDistances.py
@@ -4,10 +4,28 @@ import pandas
 import numpy
 import MDAnalysis
 
-amino_acid_3to1letter = {'CYS': 'C', 'ASP': 'D', 'SER': 'S', 'GLN': 'Q', 'LYS': 'K',
-     'ILE': 'I', 'PRO': 'P', 'THR': 'T', 'PHE': 'F', 'ASN': 'N',
-     'GLY': 'G', 'HIS': 'H', 'LEU': 'L', 'ARG': 'R', 'TRP': 'W',
-     'ALA': 'A', 'VAL':'V', 'GLU': 'E', 'TYR': 'Y', 'MET': 'M'}
+amino_acid_3to1letter = {
+    "CYS": "C",
+    "ASP": "D",
+    "SER": "S",
+    "GLN": "Q",
+    "LYS": "K",
+    "ILE": "I",
+    "PRO": "P",
+    "THR": "T",
+    "PHE": "F",
+    "ASN": "N",
+    "GLY": "G",
+    "HIS": "H",
+    "LEU": "L",
+    "ARG": "R",
+    "TRP": "W",
+    "ALA": "A",
+    "VAL": "V",
+    "GLU": "E",
+    "TYR": "Y",
+    "MET": "M",
+}
 
 
 class TrajectoryDistances(object):
@@ -28,39 +46,66 @@ class TrajectoryDistances(object):
     10th - if percentile_exlcusion is set to True, only data between the 5th and 9th percentile is considered (default=False)
 
     E.g. a = sbmlcore.StructuralDistances('tests/5uh6.gro',['tests/5uh6.xtc'], '5hu6.pdb', 'resname RFP', 'RFP_distance', distance_type='median', offsets = {'A': 0, 'B': 0, 'C': -6}, 'start_time=1000', 'end_time=49000', percentile_exclusion=True)
-    
+
     Returns
     -------
     Instantiating the class instantiates a df with segment ids, residue ids, residue names, and associated distances to the specified reference selection
-    
+
     add_feature adds distances to existing mutation dataframe, and returns new joined dataframe
 
     """
 
-    def __init__(self, pdb_file, trajectory_list, static_pdb, distance_selection, distance_name, distance_type='mean', offsets=None, start_time=None, end_time=None, percentile_exclusion=False):
+    def __init__(
+        self,
+        pdb_file,
+        trajectory_list,
+        static_pdb,
+        distance_selection,
+        distance_name,
+        distance_type="mean",
+        offsets=None,
+        start_time=None,
+        end_time=None,
+        percentile_exclusion=False,
+    ):
 
         # check files exists
-        assert pathlib.Path(pdb_file).is_file(), "File does not exist!" 
+        assert pathlib.Path(pdb_file).is_file(), "File does not exist!"
         assert pathlib.Path(static_pdb).is_file(), "File does not exist!"
 
-        #ensure trajectories are provided as a list
-        assert isinstance(trajectory_list, list), 'trajectories not provided as a list'
+        # ensure trajectories are provided as a list
+        assert isinstance(trajectory_list, list), "trajectories not provided as a list"
         for i in trajectory_list:
             assert pathlib.Path(i).is_file(), "File does not exist!"
 
-        #ensure the distance type is legal, and the distance selection and name are strings 
-        assert distance_type in ['mean', 'min', 'max', 'median'], 'provided distance_type not recognised!'
-        assert isinstance(distance_selection, str), "Distance selection must be a string!"
+        # ensure the distance type is legal, and the distance selection and name are strings
+        assert distance_type in [
+            "mean",
+            "min",
+            "max",
+            "median",
+        ], "provided distance_type not recognised!"
+        assert isinstance(
+            distance_selection, str
+        ), "Distance selection must be a string!"
         assert isinstance(distance_name, str), "Distance name must be a string!"
-        #checks whether percentile_exlcusion is True/False
-        assert isinstance(percentile_exclusion, bool), "Confidence interval must be True or False"
+        # checks whether percentile_exlcusion is True/False
+        assert isinstance(
+            percentile_exclusion, bool
+        ), "Confidence interval must be True or False"
 
-        #ensure start and end times are legal floats
+        if offsets is not None:
+            # ensure the offsets are specified in a dictionary
+            assert isinstance(
+                offsets, dict
+            ), "Offsets should be specified as a dictionary e.g. offsets = {'A': 3, 'B': -4}"
+
+        # ensure start and end times are legal floats
         if start_time is not None:
-            assert start_time>0
+            assert start_time > 0
             assert isinstance(start_time, float)
         if end_time is not None:
-            assert end_time>0
+            assert end_time > 0
             assert isinstance(end_time, float)
             if start_time is not None:
                 assert start_time < end_time
@@ -75,7 +120,9 @@ class TrajectoryDistances(object):
             reference_com = u.select_atoms(distance_selection).center_of_mass()
 
             # check atom selection exists
-            assert u.select_atoms(distance_selection).n_atoms > 0, "Atom selection does not exist! Is your selection using the correct MDAnalysis syntax?"
+            assert (
+                u.select_atoms(distance_selection).n_atoms > 0
+            ), "Atom selection does not exist! Is your selection using the correct MDAnalysis syntax?"
 
             Ca_all = u.select_atoms("name CA")
 
@@ -90,62 +137,61 @@ class TrajectoryDistances(object):
                 if end_time is not None and ts.time > end_time:
                     continue
 
-                distances = MDAnalysis.lib.distances.distance_array(reference_com, Ca_all.positions)
+                distances = MDAnalysis.lib.distances.distance_array(
+                    reference_com, Ca_all.positions
+                )
 
                 if first_pass:
                     distance_array = distances
                     first_pass = False
                 else:
                     distance_array = numpy.concatenate([distance_array, distances])
-            
-            #inverts the array to make subseqeunt calculations more intuitive
-            distance_array = distance_array.T
 
-            def exclude_percentiles(data):
-                '''returns input without 5% tails'''
-                data_list = []
-                for resnum in range(len(data)):
-                    arr = data[resnum]
-                    p5 = numpy.percentile(arr, 5)
-                    p95 = numpy.percentile(arr, 95)
-                    data_list.append(arr[(arr>=p5)&(arr<=p95)])
-                data_arr = numpy.array(data_list)
-                return data_arr
+        print(len(distance_array))
 
-            if percentile_exclusion==True:
-                distance_array = exclude_percentiles(distance_array)
+        # inverts the array to make subseqeunt calculations more intuitive
+        distance_array = distance_array.T
+        print(len(distance_array[0]))
 
-            #calculates distances as per the specified distance type
-            if distance_type == 'mean':
-                distances = numpy.mean(distance_array, axis=1)
-            elif distance_type == 'min':
-                distances = numpy.min(distance_array, axis=1)
-            elif distances == 'max':
-                distances = numpy.max(distance_array, axis=1)
-            elif distances == 'median':
-                distances = numpy.median(distance_array, axis=1)
+        if percentile_exclusion == True:
+            distance_array = TrajectoryDistances._exclude_percentiles(distance_array)
 
-            #Pulls segment ids from the static pdb file 
-            segids = [i for i in u_static.select_atoms('protein').residues.segids]
-            
-            #constructs the dictionary containihg the distances and assocaited residue labels
-            Ca_data = {'segid': segids, 'resid': Ca_all.resids, 'resname': Ca_all.resnames,
-                       distance_name: distances[0]}
+        # calculates distances as per the specified distance type
+        if distance_type == "mean":
+            distances = numpy.mean(distance_array, axis=1)
+        elif distance_type == "min":
+            distances = numpy.min(distance_array, axis=1)
+        elif distances == "max":
+            distances = numpy.max(distance_array, axis=1)
+        elif distances == "median":
+            distances = numpy.median(distance_array, axis=1)
 
-            def one_letter(row):
-                return(amino_acid_3to1letter[row.resname])
-            
-            results = pandas.DataFrame(Ca_data)
-            results['amino_acid'] = results.apply(one_letter, axis=1)
+        # Pulls segment ids from the static pdb file
+        segids = [i for i in u_static.select_atoms("protein").residues.segids]
 
-            self.results = results
-            self.distance_name = distance_name
-        
+        # constructs the dictionary containihg the distances and assocaited residue labels
+        Ca_data = {
+            "segid": segids,
+            "resid": Ca_all.resids,
+            "resname": Ca_all.resnames,
+            distance_name: distances,
+        }
+
+        def one_letter(row):
+            return amino_acid_3to1letter[row.resname]
+
+        results = pandas.DataFrame(Ca_data)
+        results["amino_acid"] = results.apply(one_letter, axis=1)
+
+        self.results = results
+        self.distance_name = distance_name
+
+        print(results)
 
     def add_feature(self, existing_df):
         """
         Adds distances to existing mutation dataframe, and returns new joined dataframe.
-        
+
         Parameters
         ---------
         1st - existing dataframe
@@ -159,31 +205,41 @@ class TrajectoryDistances(object):
 
         """
 
-        assert isinstance(existing_df, pandas.DataFrame), "You must be adding the extra feature to an existing dataframe!"
+        assert isinstance(
+            existing_df, pandas.DataFrame
+        ), "You must be adding the extra feature to an existing dataframe!"
 
-        assert self.distance_name not in existing_df.columns, "You've already added that feature!"
+        assert (
+            self.distance_name not in existing_df.columns
+        ), "You've already added that feature!"
 
-        assert 'mutation' in existing_df.columns, "Passed dataframe must contain a column called mutation"
+        assert (
+            "mutation" in existing_df.columns
+        ), "Passed dataframe must contain a column called mutation"
 
-        assert 'segid' in existing_df.columns, "Passed dataframe must contain a column called segid containing chain information e.g. A"
+        assert (
+            "segid" in existing_df.columns
+        ), "Passed dataframe must contain a column called segid containing chain information e.g. A"
 
         # Identifies the original one letter resname and resid (consistent with offset) so that the new feature can be subsequently linked to these
         def split_mutation(row):
             return pandas.Series([row.mutation[0], int(row.mutation[1:-1])])
 
-        existing_df[['amino_acid', 'resid']] = existing_df.apply(split_mutation, axis=1)
+        existing_df[["amino_acid", "resid"]] = existing_df.apply(split_mutation, axis=1)
 
-        #Create MultiIndex using segid, resid and amino_acid
-        existing_df.set_index(['segid', 'resid', 'amino_acid'], inplace=True)
-        self.results.set_index(['segid', 'resid', 'amino_acid'], inplace=True)
+        # Create MultiIndex using segid, resid and amino_acid
+        existing_df.set_index(["segid", "resid", "amino_acid"], inplace=True)
+        self.results.set_index(["segid", "resid", "amino_acid"], inplace=True)
 
-        new_df = existing_df.join(self.results, how='left')
+        new_df = existing_df.join(self.results, how="left")
 
-        half_data = len(new_df[self.distance_name])//2
-        
+        half_data = len(new_df[self.distance_name]) // 2
+
         total_nans = new_df[self.distance_name].isna().sum()
 
-        assert total_nans < half_data,  "Too many NaNs! Have you defined your offsets correctly?"
+        assert (
+            total_nans < half_data
+        ), "Too many NaNs! Have you defined your offsets correctly?"
 
         for i in new_df[self.distance_name]:
             assert isinstance(i, float), "Distances must be floats!"
@@ -191,6 +247,43 @@ class TrajectoryDistances(object):
         new_df.reset_index(inplace=True)
         self.results.reset_index(inplace=True)
 
-        new_df.drop(columns = ['amino_acid', 'resid', 'resname', 'resname_1', 'chain_offsets', 'resname_3', 'pdb_resid'], inplace=True)
+        new_df.drop(
+            columns=[
+                "amino_acid",
+                "resid",
+                "resname",
+                "resname_1",
+                "chain_offsets",
+                "resname_3",
+                "pdb_resid",
+            ],
+            inplace=True,
+        )
 
-        return(new_df)
+        return new_df
+
+    @staticmethod
+    def _exclude_percentiles(data):
+        """returns array without 5% tails"""
+        data_list = []
+        for resnum in range(len(data)):
+            arr = data[resnum]
+            p5 = numpy.percentile(arr, 5)
+            p95 = numpy.percentile(arr, 95)
+            data_list.append(arr[(arr >= p5) & (arr <= p95)])
+        data_arr = numpy.array(data_list)
+        return data_arr
+
+    @staticmethod
+    def _apply_offsets(universe, offsets):
+        """returns universe with numbering offsets applied to the atom group"""
+        for chain in offsets:
+            assert chain in set(
+                universe.residues.segids
+            ), "Need to specifify a segid that exists in the pdb!"
+            assert isinstance(
+                offsets[chain], int
+            ), "Offsets for each segid must be an integer!"
+            chainGroup = universe.select_atoms("segid " + chain)
+            chainGroup.residues.resids = chainGroup.residues.resids + offsets[chain]
+        return universe
