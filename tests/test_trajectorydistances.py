f0b08de4cffa0b43306c2b81144a866b1c2537a0
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2022-05-12 15:43:20 +0100

    #7 more testing

--- tests/test_trajectorydistances.py
+++ tests/test_trajectorydistances.py
@@ -89,11 +89,26 @@ def test_runs():
         "dist_RIF",
     )
 
+
 def test_exclude_percentiles():
-    input = numpy.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-    expected_output = numpy.array([3,4,5,6,7,8,9,10,11,12,13,14])
+    input = numpy.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15])
+    expected_output = numpy.array([3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14])
     output = sbmlcore.TrajectoryDistances._exclude_percentiles(input)
     assert output == expected_output
+
+
+def test_apply_offsets():
+    a = {"A": [1, 2, 3, 4, 5, 6], "B": [1, 2, 3, 4, 5, 6], "C": [1, 2, 3, 4, 5, 6]}
+    b = {
+        "A": [-2, -1, 0, 1, 2, 3],
+        "B": [1, 2, 3, 4, 5, 6],
+        "C": [8, 9, 10, 11, 12, 13],
+    }
+    input_df = pandas.DataFrame.from_dict(a)
+    expected_output = pandas.DataFrame.from_dict(b)
+    input_offsets = {"A": -3, "B": 0, "C": 7}
+    output = sbmlcore.TrajectoryDistances._apply_offsets(input_df, input_offsets)
+    pandas.testing.assert_frame_equal(output, expected_output)
 
 
 def test_init():
@@ -102,13 +117,13 @@ def test_init():
         [
             "tests/rpob-5uh6-3-md-1-50ns-dt10ns-nojump.xtc",
             "tests/rpob-5uh6-3-md-2-50ns-dt10ns-nojump.xtc",
-            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc"
+            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc",
         ],
         "tests/5uh6.pdb",
         "resname RFP",
         "max RFP",
         distance_type="max",
-        offsets = {'A': 0, 'B': 0, 'C': -6},
+        offsets={"A": 0, "B": 0, "C": -6},
         percentile_exclusion=True,
     ).return_dist_df()
     b = pandas.read_csv("tests/5uh6_traj_distances.csv", index_col=0)
@@ -116,22 +131,25 @@ def test_init():
 
 
 def test_add_feature():
-    #although this requires running the entire class to merge the dfs, 
-        #all other components of the class should have been individually tested by now
-    a = {'segid': ['A', 'A', 'A', 'B', 'C', 'C'], 'mutation': ['I3D','S4K', 'Q5V', 'R6D', 'S450F', 'D435F']}
+    # although this requires running the entire class to merge the dfs,
+    # all other components of the class should have been individually tested by now
+    a = {
+        "segid": ["A", "A", "A", "B", "C", "C"],
+        "mutation": ["I3D", "S4K", "Q5V", "R6D", "S450F", "D435F"],
+    }
     df = pandas.DataFrame.from_dict(a)
     b = sbmlcore.TrajectoryDistances(
         "tests/rpob-5uh6-3-warm.gro.gz",
         [
             "tests/rpob-5uh6-3-md-1-50ns-dt10ns-nojump.xtc",
             "tests/rpob-5uh6-3-md-2-50ns-dt10ns-nojump.xtc",
-            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc"
+            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc",
         ],
         "tests/5uh6.pdb",
         "resname RFP",
         "max RFP",
         distance_type="max",
-        offsets = {'A': 0, 'B': 0, 'C': -6},
+        offsets={"A": 0, "B": 0, "C": -6},
         percentile_exclusion=True,
     )
     c = sbmlcore.TrajectoryDistances(
@@ -139,21 +157,16 @@ def test_add_feature():
         [
             "tests/rpob-5uh6-3-md-1-50ns-dt10ns-nojump.xtc",
             "tests/rpob-5uh6-3-md-2-50ns-dt10ns-nojump.xtc",
-            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc"
+            "tests/rpob-5uh6-3-md-3-50ns-dt10ns-nojump.xtc",
         ],
         "tests/5uh6.pdb",
         "resname RFP",
         "mean RFP",
         distance_type="mean",
-        offsets = {'A': 0, 'B': 0, 'C': -6},
+        offsets={"A": 0, "B": 0, "C": -6},
         percentile_exclusion=True,
     )
     test_df = pandas.read_csv("tests/5uh6_added_traj_distances.csv", index_col=0)
     df = b.add_feature(df)
     df = c.add_feature(df)
     pandas.testing.assert_frame_equal(test_df, df)
-
-
-
-
-    
