8c5f45b1d31846d20fb376de0150960ededa64cf
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2022-05-16 21:57:51 +0100

    finished calculate_dihedrals method

--- sbmlcore/TrajectoryDihedrals.py
+++ sbmlcore/TrajectoryDihedrals.py
@@ -97,34 +97,55 @@ class TrajectoryDihedrals(object):
             if add_bonds:
                 protein_res = add_bonds(trajectory).select_atoms("protein")
             else:
-                protein_res = trajectory.select_atomrs("protein")
+                protein_res = trajectory.select_atoms("protein")
 
-            nonetype_angles = TrajectoryDihedrals._search_nonetypes(u)
+            self.calculate_dihedrals(trajectory, protein_res)
+            #STOPPED HERE            
 
             
 
-    def calculate_dihedral(trajectory, selection):
+    def calculate_dihedrals(self, trajectory, protein_res):
         """
         Calculates specified dihedral angles for all residues in the protein 
         and returns array of shape (timesteps, residues)
         """
 
-        selection_call = selection+'_selection()'
+        selection_call = 'res.' + self.dihedral + '_selection()'
 
-        nonetypes = TrajectoryDihedrals._search_nonetypes(trajectory, )
+        nonetypes = TrajectoryDihedrals._search_nonetypes(trajectory)
 
+        NaNs = numpy.empty((len(trajectory.trajectory), 1))
+        NaNs[:] = 0
 
-    @staticmethod
-    def _search_nonetypes(traj, selection):
+        if nonetypes[0] == 0:
+            x = NaNs.copy()
+        else:
+            x = Dihedral([eval(selection_call) for res in protein_res.residues[0:nonetypes[0]]]).run()
+            x = numpy.concatenate((x.results['angles'], NaNs), axis=1)
+        
+        for i in range(0, len(nonetypes)):
+            if i+1 < len(nonetypes):
+                y = Dihedral([eval(selection_call) for res in protein_res.residues[nonetypes[i]+1:nonetypes[i+1]]]).run()
+                x = numpy.concatenate((x, y.results['angles'], NaNs), axis=1)
+            else:
+                if nonetypes[-1] == len(protein_res.residues)-1:
+                    continue
+                else:
+                    y = Dihedral([eval(selection_call) for res in protein_res.residues[nonetypes[i]+1:]]).run()
+                    x = numpy.concatenate((x, y.results['angles']), axis=1)
+
+        return (x)
+
+    def _search_nonetypes(self, traj):
         """searches for nonetype dihedral angles and returns a list of their indexes"""
 
-        selection_call = selection+'_selection()'
+        selection_call = 'i.' + self.dihedral + '_selection()'
 
         index = 0
         nonetype_list = []
         residues = traj.select_atoms("protein")
         for i in residues.residues:
-            if not i.phi_selection():
+            if not eval(selection_call):
                 nonetype_list.append(index)
             index += 1
         return nonetype_list
@@ -139,3 +160,4 @@ class TrajectoryDihedrals(object):
         )
         traj.add_TopologyAttr("bonds", bonds)
         return traj
+
