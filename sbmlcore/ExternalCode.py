c92109382f3e2d1ca1973b5f1b400d0774d58abc
DylanAdlard <dylan.adlard@lmh.ox.ac.uk>
2022-06-23 00:17:06 +0100

    minor fix

--- sbmlcore/ExternalCode.py
+++ sbmlcore/ExternalCode.py
@@ -367,8 +367,6 @@ class SNAP2(object):
         #Create MultiIndex using segid, resid and amino_acid
         other.set_index(['segid', 'mutation_resid', 'mutated_to_resname'], inplace=True)
         self.snap2_df.set_index(['segid', 'mutation_resid', 'mutated_to_resname'], inplace=True)
-        other.to_csv('DELETE_1.CSV')
-        self.snap2_df.to_csv('DELETE_2.csv')
         other = other.join(self.snap2_df, how='left')
         other.reset_index(inplace=True) #Removes multi-index
 
