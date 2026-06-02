bd8184a30a54a1c6bf5c2a570904c6643d8b71c7
Philip W Fowler <philipwfowler@gmail.com>
2026-06-02 17:11:29 +0100

    Relax trajectory distance float comparison

--- tests/test_trajectorydistances.py
+++ tests/test_trajectorydistances.py
@@ -225,4 +225,4 @@ def test_add_feature():
 
     test_df = pandas.read_csv("tests/3fre_added_traj_distances.csv", index_col=0)
 
-    pandas.testing.assert_frame_equal(test_df, features_df)
+    pandas.testing.assert_frame_equal(test_df, features_df, rtol=1e-3, atol=1e-3)
